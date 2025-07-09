152d96209f7d6f3879a7d262a1f59619e1f8cc27
Tanishq Chhabra <tanishqchhabra98@gmail.com>
2025-07-09 22:39:52 +0530

    Graph ploting feature added

---
+++ graph_fetcher.py
@@ -0,0 +1,17 @@
+import yfinance as yf
+import matplotlib.pyplot as plt
+
+stock_symbol = input("Enter Name of stock you want last 1 month chart for: ")
+stock = yf.Ticker(stock_symbol)
+
+stock_data = stock.history(period='1mo') #1mo means 1 month
+
+print(stock_data['Close'].tail())
+
+plt.figure(figsize=(10,5)) #blank canvas for the graph
+plt.plot(stock_data.index, stock_data['Close'], marker='o', linestyle='-')
+plt.title(f'{stock_symbol} Closing price - Last 30 days')   
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.grid(True)
+plt.show()
