5de651560fc0b49b802c883819f5140127e875b3
Tanishq Chhabra <tanishqchhabra98@gmail.com>
2025-07-09 22:39:22 +0530

    Stock Price fetcher

---
+++ stock_price_fetcher.py
@@ -0,0 +1,6 @@
+import yfinance as yf
+
+stock_symbol = input("Enter Name of stock you want the Live value for: ")
+stock = yf.Ticker(stock_symbol)
+stock_data = stock.history(period='1d') #1d means 1 day
+print("Live price for ", stock_symbol, "is: ", stock_data['Close'])
